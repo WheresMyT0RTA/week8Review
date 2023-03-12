1e317e3b5c133670879cf3f81b27b6d8b4476722
WheresMyT0RTA <124740128+WheresMyT0RTA@users.noreply.github.com>
2023-03-11 18:28:29 -0800

    Create grade.sh

---
+++ grade.sh
@@ -0,0 +1,33 @@
+CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+rm -rf student-submission
+git clone $1 student-submission
+echo 'Finished cloning'
+
+cd student-submission
+if [[ -f ListExamples.java ]]
+    then 
+        echo "listExamples found"
+    else 
+        echo "listExamples not found"
+        exit 1
+fi
+
+#copied testing files to this directory
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+
+if [[ $? -eq 0 ]]
+    then 
+        echo "Program compiled"
+    else 
+        echo "Program did not compile"
+        exit 1
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+grep "Failures" output.txt
+
+
